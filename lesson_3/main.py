e52ae0f82cf26a86555907288c8593cc8342c2cc
Konstantin <poliakovkonstantin039@gmail.com>
2024-03-10 00:42:54 +0300

    0.42

---
+++ lesson_3/main.py
@@ -0,0 +1,18 @@
+firstname="Константин"
+lastname="Поляков"
+age="38"
+print(f"Привет, меня зовут {firstname} {lastname}, мне {38} лет")
+
+input_string=input("Введите строку: ")
+third_character=input_string[2]
+third_character2=input_string[:5]
+third_character3=input_string[:-2]
+print(third_character)
+print(third_character2)
+print(third_character3)
+
+some_str = "Hello, world!"
+new_str = some_str.replace("world", "Константин")
+print(new_str)
+
+
